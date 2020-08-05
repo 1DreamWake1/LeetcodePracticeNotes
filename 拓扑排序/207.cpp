88d23230be40bc861a0315e96a5c448a3ed012a2
1DreamWake1 <1127128563@qq.com>
2020-08-05 18:09:28 +0800

    课程表

--- 拓扑排序/207.cpp
+++ 拓扑排序/207.cpp
@@ -1,1 +1,38 @@
-../207.cpp
+class Solution {
+private:
+	vector<vector<int>> edges;
+	vector<int> visited;//0:没访问过，1:正在访问，2:访问过
+	bool ans = true;
+public:
+	void dfs(int v)
+	{
+		visited[v] = 1;
+		for (int i : edges[v])
+		{
+			if (visited[i] == 0)
+			{
+				dfs(i);
+				if (!ans)
+					return;
+			}
+			else if (visited[i] == 1)
+			{
+				ans = false;
+				return;
+			}
+		}
+		visited[v] = 2;
+		return;
+	}
+	bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+	{
+		edges.resize(numCourses);
+		visited.resize(numCourses, 0);
+		for (const auto& x : prerequisites)
+			edges[x[1]].push_back(x[0]);
+		for (int i = 0; i < numCourses && ans; i++)
+			if (!visited[i])
+				dfs(i);
+		return ans;
+	}
+};
