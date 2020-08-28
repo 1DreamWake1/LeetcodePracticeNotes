c423e7d3ee78e743edd56d7f6d47ebc82e53342d
1DreamWake1 <1127128563@qq.com>
2020-08-28 08:20:04 +0800

    机器人能否返回原点

---
+++ 657.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	bool judgeCircle(string moves)
+	{
+		int x = 0, y = 0;
+		for (const auto& c : moves)
+			switch (c)
+			{
+			case 'R':x++; break;
+			case 'L':x--; break;
+			case 'U':y++; break;
+			case 'D':y--; break;
+			}
+		return !(x || y);
+	}
+};
