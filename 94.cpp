9a48b6d9352d99d62fb1e7981422fd0b4c597711
1DreamWake1 <1127128563@qq.com>
2020-09-14 18:59:38 +0800

    二叉树的中序遍历

---
+++ 94.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root)
+	{
+		stack<TreeNode*> s;
+		vector<int> ans;
+		while (root || !s.empty())
+		{
+			while (root)
+			{
+				s.push(root);
+				root = root->left;
+			}
+			root = s.top();
+			s.pop();
+			ans.push_back(root->val);
+			root = root->right;
+		}
+		return ans;
+	}
+};
