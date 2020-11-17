8728f8fa28e6321682e33d7ed0b54a0dc4572d3f
1DramWake1 <1127128563@qq.com>
2020-11-17 17:21:31 +0800

    最长回文子串

--- 5.cpp
+++ 5.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-	string longestPalindrome(string s)
-	{
-		string temp = "@";
-		for (int i = 0; i < s.size(); i++)
-		{
-			temp += "#";
-			temp += s[i];
-		}
-		temp += "#$";
-		int size = temp.size();
-		std::pair<int, int> p(0, 0);
-		for (int i = 1; i < size - 1; i++)
-		{
-			int j;
-			for (j = 1; j > 0 && j < size; j++)
-				if (temp[i - j] != temp[i + j])
-					break;
-			if (j > p.first)
-			{
-				p.first = j;
-				p.second = i;
+	string longestPalindrome(string s) {
+		if (s.size() < 2)
+			return s;
+		string temp(2 * s.size() + 3, '#');
+		int m = 0, n = 0;
+		for (int i = 0; i < s.size(); ++i)
+			temp[2 * (i + 1)] = s[i];
+		temp[0] = '@';
+		temp[temp.size() - 1] = '$';
+		for (int i = 1; i < temp.size() - 1; ++i) {
+			int j = 1;
+			while (temp[i - j] == temp[i + j])
+				++j;
+			if (j > m) {
+				m = j;
+				n = i;
 			}
 		}
-		string ans1(temp, p.second - p.first + 1, 2 * p.first - 1);
-		string ans2;
-		for (int i = 0; i < ans1.size(); i++)
-			if (ans1[i] != '#')
-				ans2 += ans1[i];
-		return ans2;
+		string ans(m - 1, ' ');
+		for (int i = 0, j = n - m + 1; j < n + m; ++j)
+			if (temp[j] != '#')
+				ans[i++] = temp[j];
+		return ans;
 	}
 };
